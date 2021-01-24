54dfcdef5d87afe75372f090c835ef2f195dcd76
Phillip Khoza <phillipantonk@gmail.com>
2021-01-24 08:20:11 +0200

    dev

---
+++ js/newPage.js
@@ -0,0 +1,14 @@
+function openTwiter() {
+  window.open("https://www.w3schools.com");
+}
+function openInsta() {
+  window.open("https://www.instagram.com/swag_uafc_youth/");
+}
+function openFacebook() {
+  window.open(
+    "https://www.facebook.com/Uafc-Soshanguve-Central-Block-H-116440896879370/"
+  );
+}
+function openApp() {
+  window.open("https://wa.me/0720781237");
+}
